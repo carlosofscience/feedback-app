7fcb189035f2b5d85430b264987bfb5d9401d734
carlosofscience <carlosofscience@gmail.com>
2022-11-24 16:49:49 -0500

    adding delete feedback button

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import FeedbackList from './components/FeedbackList'
 function App() {
   
   const [feedback, seTfeedback] = useState(FeedbackData);
-  
+  const deleteFeedback = (id)=>{
+    if(window.confirm('Are you sure you want to delete this feedback?')) {
+      seTfeedback(feedback.filter((item) => item.id !== id))
+    }
+  };
+
   return (
   <>
     <Header />
     <div className="container">
-      <FeedbackList feedback={feedback}/>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
   </>
   )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,9 @@
+import {FaTimes} from 'react-icons/fa'
 import {useState} from 'react'
 import Card from './shared/Card';
 import PropTypes from 'prop-types'
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
   
   const [rating, setRating] = useState(item.rating);
   const [text, setText] = useState(item.text);
@@ -10,6 +11,7 @@ function FeedbackItem({item}) {
   return (
     <Card>
       <div className="num-display">{rating}</div>
+      <button onClick={()=>handleDelete(item.id)} className="close"><FaTimes color='purple'/></button>
       <div className="text-display">{text}</div>
     </Card>
   )
