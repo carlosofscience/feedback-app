a816ffd55a5e5d94d86afffd68cb87fdeb252c9f
carlosofscience <carlosofscience@gmail.com>
2022-11-25 02:58:44 -0500

    write review input validation created

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,19 +4,40 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setbtnDisabled] = useState(true);
+  const [message, setMessage] = useState(''); 
+
   const handleTextChange = (e) =>{
+    if(text === ''){
+      console.log(text.length);
+      setbtnDisabled(true)
+      setMessage(null)
+    }else if(text.trim().length <= 10){
+      setMessage('Text must be at least 10 characters')
+      setbtnDisabled(true)
+    }else{
+      setMessage(null)
+      setbtnDisabled(false)
+    }
+
     setText(e.target.value)
   }
 
   return (
     <Card>
       <form>
+        
         <h2>How would you rate your service with us?</h2>
+        
         {/* @todo - rationg select component */}
+        
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
+
       </form>
     </Card>
   )
