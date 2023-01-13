d475535e55ff53995a5bac84907784792c678381
carlosofscience <carlosofscience@gmail.com>
2023-01-12 19:58:31 -0500

    cleaning up comments

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 
-
-// Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-
-
 server.use(router)
 server.listen(port, () => {
   console.log("JSON Server is running at: "+port);
